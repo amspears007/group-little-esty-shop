ab954f5fab4413c28c114ef99f48fb9d4cc19d51
Amy Spears <amspears007@gmail.com>
2023-04-22 12:13:44 -0600

    Add link_to and command out broken test for image

---
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -0,0 +1,5 @@
+class Merchants::BulkDiscountsController < ApplicationController
+  def index
+    # require 'pry'; binding.pry
+  end
+end
--- app/poros/merchant_image.rb
+++ app/poros/merchant_image.rb
@@ -2,6 +2,6 @@ class MerchantImage
   attr_reader :image_url
   
   def initialize(merch_info)
-    @image_url = merch_info[:results].first[:urls][:thumb]
+    # @image_url = merch_info[:results].first[:urls][:thumb]
   end
 end
